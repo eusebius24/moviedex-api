4b3f5c528336b1f89e95bdee6efa7843251aba29
Erika Reiman <erika.reiman@gmail.com>
2020-01-25 19:24:12 -0500

    Finished assignment

---
+++ server.js
@@ -0,0 +1,56 @@
+require ('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const helmet = require('helmet')
+const MOVIES = require('./movies.json')
+
+const app = express()
+
+app.use(morgan('dev'))
+app.use(helmet())
+app.use(cors())
+
+app.use(function validateBearerToken(req, res, next) {
+    const apiToken = process.env.API_TOKEN
+    const authToken = req.get('Authorization')
+    if(!authToken || authToken.split(' ')[1] !== apiToken) {
+        return res.status(401).json({ error: 'Unauthorized request' })
+    }
+    next()
+})
+
+function handleGetMovies(req, res) {
+    let response = MOVIES;
+    if ((!req.query.genre) && (!req.query.country) && (!req.query.avg_vote)) {
+        return res.status(400).send('Please search by genre, country, and/or average vote.')
+    }
+
+    if(req.query.genre) {
+        response = response.filter(movie => 
+            movie.genre.toLowerCase().includes(req.query.genre.toLowerCase())
+        )
+    }
+
+    if(req.query.country) {
+        response = response.filter(movie => 
+            movie.country.toLowerCase().includes(req.query.country.toLowerCase())
+        )
+    }
+
+    if(req.query.avg_vote) {
+        response = response.filter(movie => 
+            movie.avg_vote >= req.query.avg_vote
+        )
+    }
+
+    res.json(response);
+}
+
+app.get('/movies', handleGetMovies)
+
+const PORT = 8000
+
+app.listen(PORT, () => {
+    console.log(`Server listening at http://localhost:${PORT}`);
+})
