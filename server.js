35220e0ff2bdd561200a1b423b19b044c3278ca4
Erika Reiman <erika.reiman@gmail.com>
2020-01-27 09:55:41 -0500

    Added Procfile

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const MOVIES = require('./movies.json')
 
 const app = express()
 
-app.use(morgan('dev'))
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+app.use(morgan(morganSetting))
 app.use(helmet())
 app.use(cors())
 
@@ -49,8 +50,18 @@ function handleGetMovies(req, res) {
 
 app.get('/movies', handleGetMovies)
 
-const PORT = 8000
+app.use((error, req, res, next) => {
+    let response
+    if (process.env.NODE_ENV === 'production') {
+      response = { error: { message: 'server error' }}
+    } else {
+      response = { error }
+    }
+    res.status(500).json(response)
+  })
+
+const PORT = process.env.PORT || 8000
 
 app.listen(PORT, () => {
-    console.log(`Server listening at http://localhost:${PORT}`);
+   
 })
